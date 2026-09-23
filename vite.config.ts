195868c00e4954a53a6dc5e80c71d0a172b8b7d9
PowSe <liprio@mail.ru>
2026-09-23 16:16:51 +0300

    feat: card

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/tets-cra/"
+  base:"/tets-cra/",
+  build: {
+    outDir: './docs',
+    emptyOutDir: true, // also necessary
+  }
 })
